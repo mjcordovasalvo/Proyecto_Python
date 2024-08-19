8e2e5b1abf4ffa74509e361e62b327fd90329e8b
mjcordovasalvo <mariajose.cordova1210@gmail.com>
2024-08-19 08:01:53 +0000

    Uso de OR

--- Condicionales.py
+++ Condicionales.py
@@ -1,10 +1,7 @@
 x=int(input("¿cual es el valor de x?"))
 y=int(input("¿cual es el valor de y?"))
 
-
-if x<y:
-    print("x es menor que y")
-elif x>y:
-    print("x es mayor que y")
+if x<y or x>y:
+    print("x no es igual a y")
 else:
-    print("x es igual que y")
+    print("x es igual a y")
