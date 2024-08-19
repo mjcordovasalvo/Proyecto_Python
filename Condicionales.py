1bd18d75dc1999b2c05b6d6438bc3e3443284ae6
mjcordovasalvo <mariajose.cordova1210@gmail.com>
2024-08-19 07:59:37 +0000

    Uso de ELIF

--- Condicionales.py
+++ Condicionales.py
@@ -1,9 +1,10 @@
 x=int(input("¿cual es el valor de x?"))
 y=int(input("¿cual es el valor de y?"))
 
+
 if x<y:
     print("x es menor que y")
-if x>y:
+elif x>y:
     print("x es mayor que y")
-if x==y:
+elif x==y:
     print("x es igual que y")
