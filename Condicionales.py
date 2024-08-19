f63b5578d997e93634e017df418a9d119cbd6051
mjcordovasalvo <mariajose.cordova1210@gmail.com>
2024-08-19 07:44:54 +0000

    Uso de varios IF

--- Condicionales.py
+++ Condicionales.py
@@ -1,5 +1,9 @@
 x=int(input("¿cual es el valor de x?"))
 y=int(input("¿cual es el valor de y?"))
 
-if x < y:
-     print("y es mayor que x")
+if x<y:
+    print("x es menor que y")
+if x>y:
+    print("x es mayor que y")
+if x==y:
+    print("x es igual que y")
