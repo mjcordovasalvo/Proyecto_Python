e7eefc062a51054d49351d86e440309ba1a98230
mjcordovasalvo <mariajose.cordova1210@gmail.com>
2024-08-27 02:04:36 +0000

    ejercicio control (sin errores)

--- cafeteria.py
+++ cafeteria.py
@@ -1,21 +1,21 @@
 precios={
     "cafe":1500,
     "te":1000,
-    "jugo de naranja": 2000
-    "pastel de chocolate": 2500
+    "jugo de naranja": 2000,
+    "pastel de chocolate": 2500,
     "tarta de frutas":3000,
 }
 
 pedidos={
     "cafe":int(input("ingrese la cantidad del cafe:")),
     "te":int(input("ingrese la cantidad del te:")),
-    "jugo de naranja":int(input("ingrese la cantidad del jugo natural:"))
-    "pastel de chocolate": int(input("ingrese la cantidad del pastel de chocolate:"))
-    "tarta de frutas"int(input("ingrese la cantidad de tarta:"))
+    "jugo de naranja":int(input("ingrese la cantidad del jugo natural:")),
+    "pastel de chocolate": int(input("ingrese la cantidad del pastel de chocolate:")),
+    "tarta de frutas":int(input("ingrese la cantidad de tarta:"))
 }
 total=0
 
-for producto, cantidad in pedidos.items()
+for producto, cantidad in pedidos.items():
     total += cantidad * precios[producto]
 
 print(total)
